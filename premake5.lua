2ebe9c59f065991a88bdeec01476f2c399a1dac9
whxtedvrknxss <kapattu333@gmail.com>
2025-05-15 16:40:41 +0100

    premake support

---
+++ premake5.lua
@@ -0,0 +1,32 @@
+project "imgui"
+  kind "StaticLib"
+  language "C++"
+
+  targetdir ("build/bin/" .. outputdir .. "/%{prj.name}")
+  objdir ("build/obj/" .. outputdir .. "/%{prj.name}")
+
+  files
+  {
+    "imconfig.h",
+    "imgui.h",
+    "imgui.cpp",
+    "imgui_draw.cpp",
+    "imgui_internals.cpp",
+    "imstb_rectpack.h",
+    "imstb_textedit.h",
+    "imstb_truetype.h",
+    "imgui_demo.cpp"
+  }
+
+  filter "system:windows"
+    systemversion "latest"
+    cppdialect "C++17"
+    staticruntime "On"
+
+  filter "configurations:Debug"
+    runtime "Debug"
+    symbols "on"
+
+  filter "configurations:Release"
+    runtime "Release"
+    optimize "on"
