8fecf9316db078968da5ff9e342b7f3229957764
whxtedvrknxss <kapattu333@gmail.com>
2025-06-18 00:27:13 +0100

    premake fix

--- premake5.lua
+++ premake5.lua
@@ -5,18 +5,35 @@ project "imgui"
   targetdir ("build/bin/" .. outputdir .. "/%{prj.name}")
   objdir ("build/obj/" .. outputdir .. "/%{prj.name}")
 
+  includedirs
+  {
+      ".",
+      IncludeDir["sdl"],
+      IncludeDir["VulkanSDK"]
+  }
+
   files
   {
-    "imconfig.h",
-    "imgui.h",
+      "backends/imgui_impl_sdl3.cpp",
+      "backends/imgui_impl_vulkan.cpp",
+      "backends/imgui_impl_dx12.cpp",
+      "backends/imgui_impl_win32.cpp",
+    --"imconfig.h",
+    --"imgui.h",
     "imgui.cpp",
     "imgui_draw.cpp",
-    "imgui_internal.h",
+    --"imgui_internal.h",
     "imgui_widgets.cpp",
-    "imstb_rectpack.h",
-    "imstb_textedit.h",
-    "imstb_truetype.h",
+    --"imstb_rectpack.h",
+    --"imstb_textedit.h",
+    "imgui_tables.cpp",
+    --"imstb_truetype.h",
     "imgui_demo.cpp"
+  }
+
+  links
+  {
+      "sdl"
   }
 
   filter "system:windows"
@@ -31,3 +48,8 @@ project "imgui"
   filter "configurations:Release"
     runtime "Release"
     optimize "on"
+
+    defines
+    {
+        "IMGUI_DISABLE_DEMO_WINDOWS"
+    }
